223bcac533094c582fdd1379e1c79482ccd67e18
StephanieMfon <okpomfonstephanie@gmail.com>
2024-04-24 23:22:43 +0100

    made updates

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,6 @@
-function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
   return promise
-    .then((success) => {
-      return { status: 200, body: "Success" };
-    })
-    .catch((error) => {
-      return new Error();
-    })
-    .finally(() => {
-      console.log("Got a response from the API");
-    });
+    .then(() => ({ status: 200, body: "success" }))
+    .catch(() => new Error())
+    .finally(console.log("Got a response from the API"));
 }
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from "./utils";
 
-function handleProfileSignup() {
+export function handleProfileSignup() {
   return Promise.all([uploadPhoto, createUser])
     .then((values) => {
       console.log(
